1e3e5f5e7da5fba4715a15540196699f331d19c4
mdshuvomolla2001 <mdshuvomolla2001@gmail.com>
2026-01-15 23:01:13 +0600

    Add files via upload

---
+++ dictionary_problem_2.py
@@ -0,0 +1,17 @@
+students_score = {
+    "shuvo": 81,
+    "asif": 75,
+    "sajid": 78,
+    "rafsan": 90,
+    "atik": 85,
+    "shakib": 70,
+    "tushin": 69,
+    "asik": 92,
+    "sadim": 88,
+    "samim": 60
+}
+
+for name, mark in students_score.items():
+    if mark>80:
+        print(name)
+
